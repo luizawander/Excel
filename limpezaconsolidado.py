d2a7d727fb4e5c8f86b44b0d83f1bc3038a813e6
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-10-03 16:15:53 -0300

    sem vinculo

--- limpezaconsolidado.py
+++ limpezaconsolidado.py
@@ -2,14 +2,15 @@ import pandas as pd
 
 df = pd.read_excel('limpezaconsolidado.xlsx', sheet_name='Limpeza')
 
-print("Antes da limpeza:")
-print(df[['RG', 'CPF']].head(10)) 
-
-df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+df = df.apply(lambda x: x.strip() if isinstance(x, str) else x)
 
 df['CPF'] = df['CPF'].map(lambda x: ''.join(filter(str.isdigit, x)) if isinstance(x, str) else x)
 df['RG'] = df['RG'].map(lambda x: ''.join(filter(str.isdigit, x)) if isinstance(x, str) else x)
+df['Contato'] = df['Contato'].map(lambda x: ''.join(filter(str.isdigit, x)) if isinstance(x, str) else x)
+df['Nascimento'] = pd.to_datetime(df['Nascimento'], errors='coerce').dt.strftime('%d/%m/%Y')
 
-print(df[['RG', 'CPF']].head(10)) 
+cpf_sem_vinculo = df[df['Vínculo'].isna() | (df['Vínculo'] == '') | (df['Vínculo'] == '#N/D')]
+
+print(cpf_sem_vinculo[['CPF', 'Vínculo']].to_string(index=False))
 
 df.to_excel('planilhalimpa.xlsx', index=False, engine='openpyxl')
