18d6637d83e8eb262fec9999d554042f39cd690a
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-10-03 15:27:31 -0300

    remoção de código de teste

--- livia.py
+++
@@ -1,9 +0,0 @@
-import openpyxl 
-
-wb = openpyxl.load_workbook('CENSO DGPE_Final.xlsm')
-
-ws = wb.active
-
-ws = wb['Base tratada']
-
-print('Total number of rows: ' + str(ws.max_row) + '. And total number of columns: ' + str(ws.max_column))
